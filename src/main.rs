d6496a2cd3f11cff08e4fc9e5fe25c555536901b
SunGrow <sungrow@tuta.io>
2022-05-17 22:07:46 +0200

    done

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ const REASON_NAMES: [&str; 61] = [
     "инородное тело носа",
 ];
 
-const REASON_IMPORTANCE: [&u32; 61] = [
+const REASON_IMPORTANCE: [&usize; 61] = [
 	&0,
 	&0,
 	&0,
@@ -134,6 +134,12 @@ const REASON_IMPORTANCE: [&u32; 61] = [
 	&4,
 ];
 
+const PATIENT_LOCATION: [&str; 3] = [ 
+    "Улица",
+    "Дом",
+    "Другое",
+];
+
 pub fn main(){
     let options = eframe::NativeOptions::default();
     eframe::run_native(
@@ -143,25 +149,58 @@ pub fn main(){
     );
 }
 
-struct CallResason {
-    name: String,
-    priority: u32,
+struct CallInfo {
+    id: usize,
+    loc_id: usize,
+    age: usize,
+    conscious: bool,
+}
+
+impl Default for CallInfo {
+    fn default() -> Self {
+        CallInfo { 
+            id: 0,
+            loc_id: 0,
+            age: 0,
+            conscious: true,
+        }
+    }
 }
 
 struct LabApp {
-    reason: CallResason,
+    call_info: CallInfo,
+    str_age: String,
+    importance: usize,
 }
+
 
 impl Default for LabApp {
     fn default() -> Self {
-        let init_reason = CallResason{
-            name: String::from(""),
-            priority: 0,
-        };
+        let info: CallInfo = Default::default();
         Self {
-            reason: init_reason
+            call_info: info,
+            str_age: "18".to_string(),
+            importance: 1,
         }
     }
+}
+
+fn dump_to_call_info(in_call_info: &mut CallInfo, in_str_age: &mut String) {
+    if in_str_age.len() > 0 {
+    in_call_info.age = in_str_age.parse::<usize>().unwrap();
+    }
+}
+
+fn calculate_importance(in_call_info: &mut CallInfo) -> usize
+{
+    let importance = REASON_IMPORTANCE[in_call_info.id] + 1;
+//    if importance == 1 && !(in_call_info.age < 18 || in_call_info.loc_id == 0 || in_call_info.conscious == false){
+//        return importance + 1;
+//    }
+    if !(in_call_info.age < 18 || in_call_info.loc_id == 0 || in_call_info.conscious == false) {
+        return importance + 1;
+    }
+    importance
 }
 
 impl eframe::App for LabApp {
@@ -169,12 +208,22 @@ impl eframe::App for LabApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Оценка категории срочности вызова");
             ui.horizontal(|ui| {
-                ui.label("Повод для вызова: ");
-                ui.add(egui::TextEdit::singleline(&mut self.reason.name)
-                       .hint_text("Поле для повода вызова")
+                ui.label("Возраст: ");
+                ui.add(egui::TextEdit::singleline(&mut self.str_age)
+                       .hint_text("Поле для ввода возраста")
                        );
-            ui.add(egui::Label::new(""));
             });
+            ui.checkbox(&mut self.call_info.conscious, "В сознании");
+            ui.label(format!("Степень срочности вызова: {}", REASON_IMPORTANCE[self.call_info.id] + 1));
+            egui::ComboBox::from_label("Причина Вызова")
+                .show_index(ui, &mut self.call_info.id, REASON_NAMES.len(), |i| REASON_NAMES[i].to_owned());
+            egui::ComboBox::from_label("Местоположение пациента")
+                .show_index(ui, &mut self.call_info.loc_id, PATIENT_LOCATION.len(), |i| PATIENT_LOCATION[i].to_owned());
+            if (ui.add(egui::Button::new("Расчитать"))).clicked() {
+                dump_to_call_info(&mut self.call_info, &mut self.str_age);
+                self.importance = calculate_importance(&mut self.call_info);
+            };
+            ui.label(format!("Категория срочности: {}", self.importance));
         });
     }
 }
